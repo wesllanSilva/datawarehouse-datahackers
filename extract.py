8cd48f4e4cc4a64353aeb0e8115f79d84312911e
Weslan Silva <wesllan90@gmail.com>
2024-06-10 23:29:39 -0300

    add arquivos para ETL

---
+++ extract.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+import pandas as pd
+from loguru import logger
+
+commodities = ['CL=F', 'GC=F', 'SI=F'] # Petróleo bruto, Ouro, Prata
+
+def buscar_dados_commodities():
+    """
+    Recebe um parametro e retorna o preco dessas acoes
+    """
+
+def buscar_todos_dados_commodities():
+    """ 
+    Concatena todos os precos de acoes
+    """ 
+
+
+logger.add("file_{time}.log")
+
+logger.info("That's it, beautiful and simple logging!")
+
+print("Isso não é um Log")
